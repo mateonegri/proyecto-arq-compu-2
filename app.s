09714ddac0b048e592e78bad173d014042c07027
Mateo Negri <mateonegri@Mateos-MacBook-Pro.local>
2023-10-09 11:37:22 -0300

    ..

--- app.s
+++ app.s
@@ -29,7 +29,8 @@ app:
 	bl pintarFondo
 
     mov x19, 0x200000  // Direccion base del array posiciones serpiente
-    mov x31, 0x400000  // Direccion base stack pointer
+    mov x29, 0x400000  // Direccion base stack pointer
+    mov x31, x29
 
 startGame:
 
