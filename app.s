5d5b3f85c447a9c0e1a0868476d93a28c525729e
Mateo Negri <mateonegri@Mateos-MacBook-Pro.local>
2023-10-11 11:23:49 -0300

    actualizarDireccion

--- app.s
+++ app.s
@@ -1,4 +1,5 @@
 .global app
+.extern inputRead
 
 // Configuraciones
     .equ SNAKE_LONGITUD_MAX, 15
@@ -42,7 +43,7 @@ startGame:
     add x1, x13, xzr
     add x1, x1, 384
 
-    str x1, [x19, 0]  // Aca se "traba" el codigo. Deja de ejecutar
+    // str x1, [x19, 0]  // Aca se "traba" el codigo. Deja de ejecutar
 
     bl pintarSerpienteInicio
 
@@ -52,7 +53,7 @@ startGame:
 
 loopGame: 
 
-    bl actualizarDireccion
+    // bl actualizarDireccion
 
     b loopGame
 
@@ -182,7 +183,7 @@ actualizarDireccion:
     // Lectura de puertos de entrada y devuelvo direccion 
     // 0 --> derecha, 1 --> izquierda, 2 --> arriba, 3 --> abajo
 
-    bl inputRead
+    // bl inputRead
 
     sub x28, x3, RIGHT
     cbz x28, derecha
